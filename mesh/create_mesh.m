6b88e8a3edcc988d79e138f64ff71643e15e55f7
Luca Pegolotti <luca.pegolotti@epfl.com>
2017-03-11 14:55:02 +0100

    Add groups to mesh

--- mesh/create_mesh.m
+++ mesh/create_mesh.m
@@ -1,4 +1,14 @@
 function [X,Y,vertices, connectivity] = create_mesh(L,H,n_elements1,n_elements2)
+% Generates rectangular mesh.
+% input= L: length
+%        H: height
+%        n_elements1: number of elements in the x direction
+%        n_elements2: number of elements in the y direction
+%
+% output= X: x coordinates of the grid
+%         Y: y coordinates of the grid
+%         vertices: vertices of the mesh
+%         connectivity: connectivity matrix
     n_elements = n_elements1 * n_elements2 * 2;
     x = linspace(0,L,n_elements1+1);
     y = linspace(0,H,n_elements2+1);
@@ -13,16 +23,61 @@ function [X,Y,vertices, connectivity] = create_mesh(L,H,n_elements1,n_elements2)
     connectivity = zeros(n_elements,4);
     
     count = 0;
-    for i = 1:n_elements2
+    i = 1;
+    for j = 1:n_elements1
+        count = count + 1;
+        if (j == n_elements1)
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 2];
+        else
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 1];
+        end
+    end
+       
+    for j = 1:n_elements1
+        count = count + 1;
+        if (j == 1)
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 4];
+        else
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 5];
+        end
+    end
+    
+    for i = 2:n_elements2-1
        for j = 1:n_elements1
           count = count + 1;
-          connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 5];
+          if (j == n_elements1)
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 2];
+          else
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 5];
+          end
        end
        
        for j = 1:n_elements1
           count = count + 1;
-          connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 5];
+          if (j == 1)
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 4];
+          else
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 5];
+          end
        end
+    end
+    i = n_elements2;
+    for j = 1:n_elements1
+        count = count + 1;
+        if (j == n_elements1)
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 2];
+        else
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+1+(i-1)*(n_elements1+1) j+1+i*(n_elements1+1) 5];
+        end
+    end
+       
+    for j = 1:n_elements1
+        count = count + 1;
+        if (j == 1)
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 4];
+        else
+            connectivity(count,:) = [j+(i-1)*(n_elements1+1) j+i*(n_elements1+1) j+i*(n_elements1+1)+1 3];
+        end
     end
     
 end
--- mesh/draw_mesh_element.m
+++ mesh/draw_mesh_element.m
@@ -1,6 +1,18 @@
 function draw_mesh_element(index,vertices,connectivity)
 
+group = connectivity(index,4);
+
 x = [vertices(connectivity(index,1),1) vertices(connectivity(index,2),1) vertices(connectivity(index,3),1)]; 
 y = [vertices(connectivity(index,1),2) vertices(connectivity(index,2),2) vertices(connectivity(index,3),2)];
 
-plot([x x(1)],[y y(1)],'k');
+if (group == 1)
+    plot([x x(1)],[y y(1)],'r','Linewidth',1);
+elseif (group == 2)
+    plot([x x(1)],[y y(1)],'b','Linewidth',1);
+elseif (group == 3)
+    plot([x x(1)],[y y(1)],'g','Linewidth',1);
+elseif (group == 4)
+    plot([x x(1)],[y y(1)],'y','Linewidth',1);
+elseif (group == 5)
+    plot([x x(1)],[y y(1)],'k');
+end

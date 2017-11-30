7fd7df883ff6106deb7a148ec486b5e72a37d1a1
Luca Pegolotti <luca.pegolotti@epfl.ch>
2017-11-30 23:29:37 +0100

    Reorganize project structure

--- addpaths.m
+++ addpaths.m
@@ -1,16 +1,4 @@
 clear all
 clc
 
-curfolder=pwd;
-addpath(genpath([curfolder,'mesh']))
-addpath(genpath([curfolder,'fespace']))
-addpath(genpath([curfolder,'matrices']))
-addpath(genpath([curfolder,'poisson']))
-addpath(genpath([curfolder,'diffusion_transport_advection']))
-addpath(genpath([curfolder,'core']))
-addpath(genpath([curfolder,'stokes']));
-addpath(genpath([curfolder,'navier_stokes']));
-addpath(genpath([curfolder,'test']));
-addpath(genpath([curfolder,'applications/reduced_basis']));
-
-
+addpath(genpath(pwd))

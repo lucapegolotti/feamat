432d7b65fdb2bfe99423df9e201250e92e5165ab
Luca Pegolotti <luca.pegolotti@epfl.ch>
2017-12-08 14:36:10 +0100

    Optimize divergence and add test

--- core/matrices/assemble_divergence.m
+++ core/matrices/assemble_divergence.m
@@ -5,10 +5,12 @@ if (derivative == 'dx')
 elseif (derivative == 'dy')
     index_der = 2;
 else
-   error([derivative, ' is not a valid derivative!']); 
+    error([derivative, ' is not a valid derivative!']);
 end
 
 connectivity_u = fespace_u.connectivity;
+connectivity_p = fespace_p.connectivity;
+
 vertices = fespace_u.mesh.vertices;
 nodes_u = fespace_u.nodes;
 nlocalfunctions_u = fespace_u.n_functions_per_element;
@@ -16,35 +18,88 @@ nlocalfunctions_u = fespace_u.n_functions_per_element;
 nodes_p = fespace_p.nodes;
 nlocalfunctions_p = fespace_p.n_functions_per_element;
 
+n_functionsqr = nlocalfunctions_u*nlocalfunctions_p;
+
 n_elements = size(connectivity_u,1);
 
 n_nodes_u = size(nodes_u,1);
 n_nodes_p = size(nodes_p,1);
 
-[gp,weights,n_gauss] = gauss_points2D(2);
+n_gauss = 3;
+[gp,weights,~] = gauss_points2D(n_gauss);
 
-B = sparse(n_nodes_p,n_nodes_u);
+elements_B = zeros(nlocalfunctions_u*nlocalfunctions_p*n_elements,1);
+indices_i = zeros(nlocalfunctions_u*nlocalfunctions_p*n_elements,1);
+indices_j = zeros(nlocalfunctions_u*nlocalfunctions_p*n_elements,1);
 
-for i = 1:n_elements
-    indices = connectivity_u(i,1:end-1);
-    x1 = vertices(indices(1),1:2)';
-    x2 = vertices(indices(2),1:2)';
-    x3 = vertices(indices(3),1:2)';
-    
-    mattransf = [x2-x1 x3-x1];
-    invmat = inv(mattransf);
-    
-    % transformation from parametric to physical
-    dettransf = abs(det(mattransf));
-    
-    for j = 1:n_gauss
-        transfgrad = invmat'*fespace_u.grads(gp(:,j));
-        transfun = fespace_p.functions(gp(:,j));
-        for k = 1:nlocalfunctions_u
-            for l = 1:nlocalfunctions_p
-                divergence_element = dettransf*(transfgrad(index_der,k))*transfun(l)*weights(j)/2;
-                B(indices(l),indices(k)) = B(indices(l),indices(k)) + divergence_element;
-            end
+
+if (~strcmp(fespace_u.mesh.type,'structured'))
+    for i = 1:n_elements
+        indices_u = connectivity_u(i,1:end-1);
+        indices_p = connectivity_p(i,1:end-1);        
+        x1 = vertices(indices_u(1),1:2)';
+        x2 = vertices(indices_u(2),1:2)';
+        x3 = vertices(indices_u(3),1:2)';
+
+        [I1,I2] = meshgrid(indices_u,indices_p);
+        currindices = (i-1)*n_functionsqr+1:n_functionsqr*i;
+        indices_i(currindices) = I2(:);
+        indices_j(currindices) = I1(:);
+        
+        mattransf = [x2-x1 x3-x1];
+        invmat = inv(mattransf);
+        
+        % transformation from parametric to physical
+        dettransf = abs(det(mattransf));
+
+        new_elements = zeros(n_functionsqr,1);
+        
+        for j = 1:n_gauss
+            transfgrad = invmat'*fespace_u.grads(gp(:,j));
+            transfun = fespace_p.functions(gp(:,j));
+
+            divergence_element = dettransf*(transfgrad(index_der,:)')*transfun(:)'*weights(j)/2;
+            new_elements = new_elements + divergence_element(:);
         end
+        elements_B(currindices) = new_elements;
+    end
+else
+    [fespace_u,~] = add_members_structured_meshes(fespace_u, n_gauss);
+    [fespace_p,~] = add_members_structured_meshes(fespace_p, n_gauss);
+    
+    divergence_elements1 = {};
+    divergence_elements2 = {};
+    for j = 1:n_gauss
+        divergence_elements1{end+1} = fespace_u.dettransf1* ...
+            (fespace_u.transfgrads1{j}(index_der,:)')*...
+            fespace_p.transffuns1{j}(:)'*weights(j)/2;
+        divergence_elements2{end+1} = fespace_u.dettransf2* ...
+            (fespace_u.transfgrads2{j}(index_der,:)')*...
+            fespace_p.transffuns2{j}(:)'*weights(j)/2;
+    end
+    sum_divergence_elements1 = divergence_elements1{1};
+    sum_divergence_elements2 = divergence_elements2{1};
+    for i = 2:n_gauss
+        sum_divergence_elements1 = sum_divergence_elements1 + divergence_elements1{i};
+        sum_divergence_elements2 = sum_divergence_elements2 + divergence_elements2{i};
+    end
+    
+    for i = 1:n_elements
+        indices_u = connectivity_u(i,1:end-1);
+        indices_p = connectivity_p(i,1:end-1);
+        
+        [I1,I2] = meshgrid(indices_u,indices_p);
+        currindices = (i-1)*n_functionsqr+1:n_functionsqr*i;
+        indices_i(currindices) = I2(:);
+        indices_j(currindices) = I1(:);
+        
+        % then the triangle is in this configuration /|
+        if (indices_u(2) == indices_u(1) + 1)
+            new_elements = sum_divergence_elements1(:);
+        else
+            new_elements = sum_divergence_elements2(:);
+        end
+        elements_B(currindices) = new_elements;
     end
 end
+B = sparse(indices_i,indices_j,elements_B,n_nodes_p,n_nodes_u);
---
+++ core/matrices/test_assemble_divergence.m
@@ -0,0 +1,31 @@
+% common variables
+
+tol = 1e-12;
+
+%% Test 1: check if size is correct
+
+mesh = create_mesh(0,0,1,1,20,20);
+fespace_u = create_fespace(mesh,'P2',[0 0 0 0]);
+fespace_p = create_fespace(mesh,'P1',[0 0 0 0]);
+
+B = assemble_divergence(fespace_u,fespace_p,'dx');
+
+assert(size(B,1) == size(fespace_p.nodes,1))
+assert(size(B,2) == size(fespace_u.nodes,1))
+
+%% Test 2: check if optimized code is correct
+
+mesh = create_mesh(0,0,1,1,20,20);
+fespace_u = create_fespace(mesh,'P2',[0 0 0 0]);
+fespace_p = create_fespace(mesh,'P1',[0 0 0 0]);
+
+B1 = assemble_divergence(fespace_u,fespace_p,'dx');
+
+fespace_u.mesh.type  = '';
+B2 = assemble_divergence(fespace_u,fespace_p,'dx');
+
+x = rand(size(fespace_u.nodes,1),1);
+assert(norm(B1*x - B2*x) < tol)
+
+
+

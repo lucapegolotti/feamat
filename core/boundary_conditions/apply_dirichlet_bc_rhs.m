bec29e4b3b7c4f5f59aee538d995d0ca173e7964
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-08-24 15:31:54 +0200

    Small fix in apply_dirichlet_bc

--- core/boundary_conditions/apply_dirichlet_bc_rhs.m
+++ core/boundary_conditions/apply_dirichlet_bc_rhs.m
@@ -18,10 +18,10 @@ bc_flags = fespace.bc;
 for i = 1:n_nodes
     if (nodes(i,3)~=0)
         if (bc_flags(nodes(i,3)))
-            vd = dirichlet_functions(nodes(i,:)');
+            vd = dirichlet_functions(nodes(i,1:2)');
             b(i) = vd(nodes(i,3));
         elseif (nodes(i,4) ~= 0 && bc_flags(nodes(i,4)))
-            vd = dirichlet_functions(nodes(i,:)');
+            vd = dirichlet_functions(nodes(i,1:2)');
             b(i) = vd(nodes(i,4));
         end
     end

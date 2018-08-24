3e8c81c3e32bf1925ab4c6ea12cdae86a4a42350
Luca Pegolotti <luca.pegolotti@epfl.ch>
2018-08-24 19:42:25 +0200

    Modify draw_mesh to use trimesh

--- tools/plot/draw_mesh.m
+++ tools/plot/draw_mesh.m
@@ -2,34 +2,15 @@ function draw_mesh(mesh,varargin)
 % Draw the mesh (warning: this function is yet to be optimized)
 % input=
 %           mesh: mesh to draw
-%           (optional)
-%           'color' to color the boundary elements with different colors
 %
 
-color = 0;
-if (nargin == 2)
-    if (strcmp(varargin{1},'color'))
-        color = 1;
-    else
-        error('Unknown parameter to draw_mesh function!');
-    end
+if (nargin == 1)
+    color = [0.3 0.3 0.3];
+else
+    color = varargin{1};
 end
 
-n_el = size(mesh.elements,1);
-
-if (~color)
-    for i = 1:n_el
-        draw_mesh_element(i,mesh.vertices,mesh.elements)
-        hold on
-    end
-elseif (color)
-    for i = 1:n_el
-        draw_mesh_element(i,mesh.vertices,mesh.elements,'color')
-        hold on
-    end
-end
+trimesh(mesh.elements,mesh.vertices(:,1),mesh.vertices(:,2),'Color',color);
 
 axis equal
 axis([mesh.xp mesh.xp+mesh.L mesh.yp mesh.yp+mesh.H]);
-
-end

51fde92c1f4e74ca94acd65001979d9ff6b575a1
Riccardo Tenderini <riccardo.tenderini@epfl.ch>
2019-11-10 10:34:09 +0100

    Added coefficients for stable BDF5 and BDF6

--- core/matrices/get_multistep_coefficients.m
+++ core/matrices/get_multistep_coefficients.m
@@ -3,13 +3,13 @@ function [coeff] = get_multistep_coefficients(step_number, method, varargin)
 % Adams-Moulton time marching scheme
 %Input =
 %          step_number: number of steps (supposed to be >=1)
-%          method: update scheme used (either AM or BDF)
+%          method: update scheme used (either AM, Theta or BDF)
 %          varargin: if step number is 1 and varargin is passed, it
-%          represents the theta of the one-step theta method
+%          represents the theta of the one-step Theta method
 %Output =
 %         coeff: coefficients characterizing the multistep method
 
-assert(step_number>=1 && step_number<=4)
+assert(step_number>=1 && step_number<=6)
 
 if step_number == 1
     if nargin > 1
@@ -19,30 +19,76 @@ if step_number == 1
     end
     
 elseif step_number == 2
-    if strcmp(method, "AM")==1
+    if strcmp(method, "AM")
         coeff = [5/12, 8/12, -1/12];
-    elseif strcmp(method, "BDF")==1
+    elseif strcmp(method, "BDF")
         coeff = [2/3, -4/3, 1/3];
     else
         disp("ERROR: unrecognized multistep time marching scheme")
+        msgID = 'myComponent:valueError';
+        msgtext = ['Unknown value inserted for the time marching scheme. Inserted value: ', method];
+        ME = MException(msgID,msgtext);
+        throw(ME)
     end
         
 elseif step_number == 3
-    if strcmp(method, "AM")==1
+    if strcmp(method, "AM")
         coeff = [9/24, 19/24, -5/24, 1/24];
-    elseif strcmp(method, "BDF")==1
+    elseif strcmp(method, "BDF")
         coeff = [6/11, -18/11, 9/11, -2/11];
     else
         disp("ERROR: unrecognized multistep time marching scheme")
+        msgID = 'myComponent:valueError';
+        msgtext = ['Unknown value inserted for the time marching scheme. Inserted value: ', method];
+        ME = MException(msgID,msgtext);
+        throw(ME)
     end
 
 elseif step_number == 4
-    if strcmp(method, "AM")==1
+    if strcmp(method, "AM")
         coeff = [251/720, 646/720, -264/720, 106/720, -19/720];
-    elseif strcmp(method, "BDF")==1
+    elseif strcmp(method, "BDF")
         coeff = [12/25, -48/25, 36/25, -16/25, 3/25];
     else
         disp("ERROR: unrecognized multistep time marching scheme")
+        msgID = 'myComponent:valueError';
+        msgtext = ['Unknown value inserted for the time marching scheme. Inserted value: ', method];
+        ME = MException(msgID,msgtext);
+        throw(ME)
+    end
+   
+elseif step_number == 5
+    if strcmp(method, "AM")
+        disp("ERROR: AM multistep method with 5 steps is not implemented")
+        msgID = 'myComponent:valueError';
+        msgtext = 'Invalid value inserted.';
+        ME = MException(msgID,msgtext);
+        throw(ME)
+    elseif strcmp(method, "BDF")
+        coeff = [60/137, -300/137, 300/137, -200/137, 75/137, -12/137];
+    else
+        disp("ERROR: unrecognized multistep time marching scheme")
+        msgID = 'myComponent:valueError';
+        msgtext = ['Unknown value inserted for the time marching scheme. Inserted value: ', method];
+        ME = MException(msgID,msgtext);
+        throw(ME)
+    end
+ 
+elseif step_number == 6
+    if strcmp(method, "AM")
+        disp("ERROR: AM multistep method with 6 steps is not implemented")
+        msgID = 'myComponent:valueError';
+        msgtext = 'Invalid value inserted.';
+        ME = MException(msgID,msgtext);
+        throw(ME)
+    elseif strcmp(method, "BDF")
+        coeff = [60/147, -360/147, 450/147, -400/147, 225/147, -72/147, 10/147];
+    else
+        disp("ERROR: unrecognized multistep time marching scheme")
+        msgID = 'myComponent:valueError';
+        msgtext = ['Unknown value inserted for the time marching scheme. Inserted value: ', method];
+        ME = MException(msgID,msgtext);
+        throw(ME)
     end
     
 end

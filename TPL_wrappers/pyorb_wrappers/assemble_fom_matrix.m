68c7814c4c1594ec3db6e152bb86e9f8d867924d
Riccardo Tenderini <riccardo.tenderini@epfl.ch>
2020-01-02 21:20:46 +0100

    Update fom matrix assembling; mass matrix can be costructed also from steady problems to compute L2 norms

--- TPL_wrappers/pyorb_wrappers/assemble_fom_matrix.m
+++ TPL_wrappers/pyorb_wrappers/assemble_fom_matrix.m
@@ -1,47 +1,45 @@
 function [array] = assemble_fom_matrix( param, fem_specifics, varargin )
-% Assemble fom matrix for elliptic scalar problems
+% Assemble fom matrices A and M
 % input=
-%           param: vector of parameters
+%           param: vector of parameters; if 1, default stiffness matrix is
+%           constructed
 %           fem_specifics: struct containing the information to build the
 %           mesh and the fespace
 % output=
-%           array: struct containing the stiffness matrix in COO format
+%           array: struct containing the mass and stiffness matrices in COO format
 
-    [~, fespace] = set_fem_simulation( fem_specifics );
+    [~, fespace] = set_fem_simulation( fem_specifics, [1,1,0,1]);
 
     dirichlet_functions = @(x) [0;0;0;0];
-    neumann_functions = @(x) [1;0;0;0];
+    neumann_functions = @(x) [0;0;0;0];
 
     % forcing term (not employed)
     f = @(x) 0*x(1,:);
 
     current_model = fem_specifics.model;
-
-    mu = build_diffusion( param, current_model );
+    
+    if param == 1
+        mu = param;
+    else
+        mu = build_diffusion( param, current_model );
+    end
     
     if nargin == 2
-        [ A, ~ ] = assembler_poisson( fespace, f, mu,dirichlet_functions, neumann_functions );
-        if(isfield(fem_specifics,'final_time'))
-            M = assemble_mass(fespace);
-        end
+        [ A, ~ ] = assembler_poisson( fespace, f, mu, dirichlet_functions, neumann_functions );
+        M = assemble_mass(fespace);
     else
         element_list = varargin{1};
-        [ A, ~ ] = assembler_poisson( fespace, f, mu,dirichlet_functions, neumann_functions, element_list );
-        if(isfield(fem_specifics,'final_time'))
-            M = assemble_mass(fespace,1,element_list);
-        end
+        [ A, ~ ] = assembler_poisson( fespace, f, mu, dirichlet_functions, neumann_functions, element_list );
+        M = assemble_mass(fespace,1,element_list);
     end
     
     if nargin < 4
         
        [ i, j, val ] = find( A );
        array.A = [ i, j, val ];
-       if(isfield(fem_specifics,'final_time'))
            
-           [ i, j, val ] = find( M );
-           array.M = [ i, j, val ];
-
-       end
+       [ i, j, val ] = find( M );
+       array.M = [ i, j, val ];
        
     elseif nargin == 4
         
@@ -51,18 +49,13 @@ function [array] = assemble_fom_matrix( param, fem_specifics, varargin )
             elements_A(ii) = A(indices_list(ii, 1), indices_list(ii, 2));
         end       
         array.A = [indices_list, elements_A];
-        
-        if(isfield(fem_specifics,'final_time'))
-            elements_M = zeros( size( indices_list, 1 ), 1 );
+        elements_M = zeros( size( indices_list, 1 ), 1 );
         for ii = 1:size( indices_list , 1 )
             elements_M(ii) = M(indices_list(ii, 1), indices_list(ii, 2));
         end       
-        array.M = [indices_list, elements_M];    
-        end
+        array.M = [indices_list, elements_M];   
         
     end
-    
-
     
 end
 
